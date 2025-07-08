202261bd351296bc895d069b5552b8e6ee6d8c13
Sven Krieger <37476281+svkrieger@users.noreply.github.com>
2025-07-08 15:11:51 +0200

    Configure new status health check
    Introduce config parameter 'cc.use_status_check'. When enabled, monit will use the /internal/v4/status endpoint to determine
    whether CC is healthy. If the endpoint returns unhealthy or if the endpoint did not return OK for 600 seconds, a restart will be triggered.

--- spec/cloud_controller_ng/healthcheck_spec.rb
+++ spec/cloud_controller_ng/healthcheck_spec.rb
@@ -20,12 +20,31 @@ module Bosh
             expect(rendered_file).to include('--retry 5')
           end
 
+          it 'uses the healthz endpoint' do
+            rendered_file = template.render({}, consumes: {})
+            expect(rendered_file).to include('/healthz')
+          end
+
           context 'when custom values are provided' do
             it 'renders the provided values' do
               rendered_file = template.render({ 'cc' => { 'ccng_monit_http_healthcheck_timeout_per_retry' => 30,
                                                           'ccng_monit_http_healthcheck_retries' => 2 } }, consumes: {})
               expect(rendered_file).to include('--max-time 30')
               expect(rendered_file).to include('--retry 2')
+            end
+          end
+
+          context 'when "use_status_check" is set to true' do
+            it 'uses the /v4/internal/status endpoint' do
+              rendered_file = template.render({ 'cc' => { 'use_status_check' => true } }, consumes: {})
+              expect(rendered_file).to include('/internal/v4/status')
+            end
+
+            context 'when Thin is used' do
+              it 'uses the healthz endpoint' do
+                rendered_file = template.render({ 'cc' => { 'use_status_check' => true, 'webserver' => 'thin' } }, consumes: {})
+                expect(rendered_file).not_to include('/healthz')
+              end
             end
           end
         end
