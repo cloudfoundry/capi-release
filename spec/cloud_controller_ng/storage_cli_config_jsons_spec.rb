4e0ecf6574b7a2760d5cb118b88feb9731364033
Katharina Przybill <30441792+kathap@users.noreply.github.com>
2025-10-29 12:39:03 +0100

    stop rendering in pre-start, use bosh templates

--- spec/cloud_controller_ng/storage_cli_config_jsons_spec.rb
+++ spec/cloud_controller_ng/storage_cli_config_jsons_spec.rb
@@ -5,10 +5,10 @@ require 'yaml'
 require 'bosh/template/test'
 
 TEMPLATES = {
-  droplets: ['config/blobstore_configs.json', %w[cc droplets connection_config]],
-  buildpacks: ['config/blobstore_configs.json', %w[cc buildpacks connection_config]],
-  packages: ['config/blobstore_configs.json', %w[cc packages connection_config]],
-  resource_pool: ['config/blobstore_configs.json', %w[cc resource_pool connection_config]]
+  droplets: ['config/storage_cli_config_droplets.json', %w[cc droplets connection_config]],
+  buildpacks: ['config/storage_cli_config_buildpacks.json', %w[cc buildpacks connection_config]],
+  packages: ['config/storage_cli_config_packages.json', %w[cc packages connection_config]],
+  resource_pool: ['config/storage_cli_config_resource_pool.json', %w[cc resource_pool connection_config]]
 }.freeze
 
 module Bosh
@@ -16,23 +16,21 @@ module Bosh
     module Test
       RSpec.describe 'storage-cli JSON templates' do
         let(:release_path) { File.join(File.dirname(__FILE__), '../..') }
-        let(:release)      { ReleaseDir.new(release_path) }
-        let(:job)          { release.job('cloud_controller_ng') }
-        let(:links)        { {} }
-
-        # Default all scopes to non-Azure so only the scope under test must be populated
+        let(:release) { ReleaseDir.new(release_path) }
+        let(:job) { release.job('cloud_controller_ng') }
+        let(:links) { {} }
         let(:props) do
           {
             'cc' => {
-              'droplets' => { 'connection_config' => {}, 'blobstore_provider' => 'S3' },
-              'buildpacks' => { 'connection_config' => {}, 'blobstore_provider' => 'S3' },
-              'packages' => { 'connection_config' => {}, 'blobstore_provider' => 'S3' },
-              'resource_pool' => { 'connection_config' => {}, 'blobstore_provider' => 'S3' }
+              'droplets' => { 'connection_config' => {}, 'blobstore_provider' => 'AzureRM' },
+              'buildpacks' => { 'connection_config' => {}, 'blobstore_provider' => 'AzureRM' },
+              'packages' => { 'connection_config' => {}, 'blobstore_provider' => 'AzureRM' },
+              'resource_pool' => { 'connection_config' => {}, 'blobstore_provider' => 'AzureRM' }
             }
           }
         end
 
-        TEMPLATES.each do |scope, (template_path, keypath)|
+        TEMPLATES.each_value do |(template_path, keypath)|
           describe template_path do
             let(:template) { job.template(template_path) }
 
@@ -41,54 +39,41 @@ module Bosh
               path[0..-2].each { |key| cursor = (cursor[key] ||= {}) }
               cursor[path.last] = value
             end
-
-            context "when provider is AzureRM for #{scope}" do
-              before do
-                props['cc'][scope.to_s]['blobstore_provider'] = 'AzureRM'
-              end
-
-              it 'renders and normalizes put_timeout_in_seconds to "41" when blank' do
-                set(props, keypath, {
-                      'provider' => 'AzureRM',
-                      'azure_storage_account_name' => 'acc',
-                      'azure_storage_access_key' => 'key',
-                      'container_name' => 'cont',
-                      'put_timeout_in_seconds' => ''
-                    })
-
-                full = YAML.safe_load(template.render(props, consumes: links))
-                json = full.fetch(scope.to_s)
-                expect(json).to include(
-                  'provider' => 'AzureRM',
-                  'account_name' => 'acc',
-                  'account_key' => 'key',
-                  'container_name' => 'cont',
-                  'put_timeout_in_seconds' => '41'
-                )
-              end
-
-              it 'keeps existing put_timeout_in_seconds when provided' do
-                set(props, keypath, {
-                      'provider' => 'AzureRM',
-                      'azure_storage_account_name' => 'acc',
-                      'azure_storage_access_key' => 'key',
-                      'container_name' => 'cont',
-                      'put_timeout_in_seconds' => '7'
-                    })
-
-                full = YAML.safe_load(template.render(props, consumes: links))
-                json = full.fetch(scope.to_s)
-                expect(json['put_timeout_in_seconds']).to eq('7')
-              end
+            it 'renders and normalizes put_timeout_in_seconds to "41" when blank' do
+              set(props, keypath, {
+                    'provider' => 'AzureRM',
+                    'azure_storage_account_name' => 'acc',
+                    'azure_storage_access_key' => 'key',
+                    'container_name' => 'cont',
+                    'put_timeout_in_seconds' => ''
+                  })
+              json = YAML.safe_load(template.render(props, consumes: links))
+              expect(json).to include(
+                'provider' => 'AzureRM',
+                'account_name' => 'acc',
+                'account_key' => 'key',
+                'container_name' => 'cont',
+                'put_timeout_in_seconds' => '41'
+              )
             end
 
-            context "when provider is non-Azure for #{scope}" do
-              it 'renders {}' do
-                # leave default 'S3' for this scope
-                full = YAML.safe_load(template.render(props, consumes: links))
-                json = full.fetch(scope.to_s)
-                expect(json).to eq({})
-              end
+            it 'keeps existing put_timeout_in_seconds when provided' do
+              set(props, keypath, {
+                    'provider' => 'AzureRM',
+                    'azure_storage_account_name' => 'acc',
+                    'azure_storage_access_key' => 'key',
+                    'container_name' => 'cont',
+                    'put_timeout_in_seconds' => '7'
+                  })
+              json = YAML.safe_load(template.render(props, consumes: links))
+              expect(json['put_timeout_in_seconds']).to eq('7')
+            end
+
+            it 'renders {} for non-Azure providers' do
+              keypath[0..-2].reduce(props) { |acc, elem| acc[elem] ||= {} }['blobstore_provider'] = 'S3'
+
+              json = YAML.safe_load(template.render(props, consumes: links))
+              expect(json).to eq({})
             end
           end
         end
