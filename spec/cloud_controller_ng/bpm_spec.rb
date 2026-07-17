bb96815f6297ff637b850bf44760cd5199c2909d
Katharina Przybill <30441792+kathap@users.noreply.github.com>
2026-07-17 10:12:23 +0200

    Remove fog_connection, log_fog_requests and FOG_DEBUG — complete fog removal

--- spec/cloud_controller_ng/bpm_spec.rb
+++ spec/cloud_controller_ng/bpm_spec.rb
@@ -11,9 +11,7 @@ module Bosh
       describe 'bpm job template rendering' do
         def expect_default_debug_env_vars(env_vars)
           expect(env_vars).to have_key('DEBUG')
-          expect(env_vars).to have_key('FOG_DEBUG')
           expect(env_vars['DEBUG']).to be(true)
-          expect(env_vars['FOG_DEBUG']).to be(true)
         end
 
         def valkey_volume_mounted?(process)
@@ -35,37 +33,8 @@ module Bosh
         let(:release) { ReleaseDir.new(release_path) }
         let(:job) { release.job('cloud_controller_ng') }
 
-        let(:properties_debug_fog) do
-          { 'cc' => { 'log_fog_requests' => true, 'packages' => { 'fog_connection' => { 'provider' => 'AWS' } } } }
-        end
-        let(:properties_without_debug) do
-          { 'cc' => { 'log_fog_requests' => false, 'packages' => { 'fog_connection' => { 'provider' => 'foo' } } } }
-        end
-
         describe 'config/bpm.yml' do
           let(:template) { job.template('config/bpm.yml') }
-
-          context 'when fog debug logging is enabled' do
-            it 'sets the DEBUG env var' do
-              template_hash = YAML.safe_load(template.render(properties_debug_fog, consumes: {}))
-
-              results = template_hash['processes'].select { |p| p['name'].include?('cloud_controller_ng') }
-              expect(results.length).to eq(1)
-              expect_default_debug_env_vars(results[0]['env'])
-            end
-          end
-
-          context 'when fog debug logging is disabled' do
-            it 'sets not any debug env var' do
-              template_hash = YAML.safe_load(template.render(properties_without_debug, consumes: {}))
-
-              results = template_hash['processes'].select { |p| p['name'].include?('cloud_controller_ng') }
-              expect(results.length).to eq(1)
-              env_vars = results[0]['env']
-              expect(env_vars).not_to have_key('DEBUG')
-              expect(env_vars).not_to have_key('FOG_DEBUG')
-            end
-          end
 
           describe 'valkey config' do
             it 'mounts the valkey volume into the ccng job container' do
